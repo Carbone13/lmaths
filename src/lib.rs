b48ecb87ffbc0de8d2320c6139c430ad0e741258
Lucas Michaudel <lucas.michaudel@gmail.com>
2022-04-06 15:45:54 +0200

    re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ pub mod point2;
 pub mod vector2;
 
 pub use self::vector2::*;
+pub use self::Vector2;
 pub use self::vector2::dot;
 pub use self::vector2::triple_product;
 pub use self::point2::*;
+pub use self::Point2;
